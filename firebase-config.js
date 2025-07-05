2c2dd85a4e64cc18d8a02d715ea2cb9b0adc80bd
Bhone Pyae Sone <bhone.ps@proton.me>
2025-07-05 14:43:32 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Paste your Firebase config here from Step 2
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB3zuiocS35sQSCCrqqizLtrZRBhGbkMq8",
+  authDomain: "vozz-os-cafe-restaurant.firebaseapp.com",
+  projectId: "vozz-os-cafe-restaurant",
+  storageBucket: "vozz-os-cafe-restaurant.firebasestorage.app",
+  messagingSenderId: "1057708941213",
+  appId: "1:1057708941213:web:f2a36e65620d6ab20e9974"
 };
 
 export default firebaseConfig;
